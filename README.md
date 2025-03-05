be6677279d64b24587a145060c915c72d6cc8f59
Mads B. Kjær <mbk1dk@gmail.com>
2025-03-05 11:11:18 +0100

    Added log file for changes made to data entries

---
+++ README.md
@@ -0,0 +1,18 @@
+Week 2 assignment:
+    Exercise 1:
+        Run sort_names.py to sort names alphabetically, by length or both.
+        Takes "a", "l" or "al" as console input.
+
+        Run letter_counter.py to get the combined count of each letter (not case sensitive).
+
+    Exercise 2:
+        Run log_filter.py to filter the log file by keyword.
+        Takes keyword as console input (not case sensitive).
+
+    Exercise 3:
+        Run data_migration.py to create file with "corrected" data entries and a file with complete data entries after correction.
+        Additionally a log file is created for the changes made to the data entries.
+
+        I did struggle a bit with finding smart ways to identify errors and the many different scenarios quickly made my code hard to manage.
+
+
--- opg3/data_migration.py
+++ opg3/data_migration.py
@@ -35,8 +35,6 @@ def id_correction(row: list[str], index: int, used_ids: list[int]) -> None:
 
 
 def mail_correction(row: list[int], index: int) -> None:
-    if row[2] == "":
-        return
     for mail_end in ["gmail.com", "yahoo.com", "hotmail.com"]:
         if mail_end in row[2]:
             row[2] = row[2][: row[2].find(mail_end)] + "@" + mail_end
@@ -45,35 +43,62 @@ def mail_correction(row: list[int], index: int) -> None:
 
 
 def error_handling(
-    row: list[str], index: int, used_ids: list[int], expected_width: int
+    row: list[str],
+    index: int,
+    used_ids: list[int],
+    expected_width: int,
+    changes_list: list[list[str]],
 ) -> list[str]:
 
     # Removes empty rows
     if len(row) == row.count(""):
+        changes_list.append(f"Line {index:5} changes: Empty cell: Removed line.\n")
         return None
 
+    changes_made: str = f"Line {index:5} changes: "
+
     if len(row) != expected_width:
+        old_width: int = len(row)
         width_correction(row, expected_width, index)
+        changes_made += f"Empty cell: Removed {old_width - expected_width} cell(s). "
 
     if row[0] != f"{index}":
+        old_id: str = row[0]
         id_correction(row, index, used_ids)
+        changes_made += f"Id: {repr(old_id)} -> {repr(row[0])}. "
 
-    if row[1].count(" ") > 1:
+    if row[1].startswith(" ") or row[1].endswith(" "):
+        old_name: str = row[1]
         row[1] = row[1].strip()
+        changes_made += f"Name: {repr(old_name)} -> {repr(row[1])}. "
 
-    if "@" not in row[2]:
+    if ("@" not in row[2]) and (not row[2] == ""):
+        old_mail: str = row[2]
         mail_correction(row, index)
+        changes_made += f"Mail: {repr(old_mail)} -> {repr(row[2])}. "
 
     if (row[1] == "") and (row[2] != ""):
         name: list[str] = row[2][: row[2].find("@")].split(".")
         row[1] = " ".join(name).title()
+        changes_made += f"Name from mail: {repr(row[1])} from {repr(row[2])}. "
 
     try:
         purchase_amount: float = float(row[3])
         if purchase_amount < 0:
+            old_purchase_amount: str = row[3]
             row[3] = row[3].lstrip("-")
+            changes_made += (
+                f"Purchase amount: {repr(old_purchase_amount)} -> {repr(row[3])}. "
+            )
     except:
+        old_purchase_amount: str = row[3]
         row[3] = ""
+        changes_made += (
+            f"Purchase amount: {repr(old_purchase_amount)} -> '{repr(row[3])}'. "
+        )
+
+    if changes_made != f"Line {index:5} changes: ":
+        changes_list.append(changes_made + "\n")
 
     return row
 
@@ -82,17 +107,24 @@ def migrate_data(
     target_file: str,
     source_file: str = path.join("Data", "source_data.csv"),
 ) -> None:
+
+    used_ids: list[int] = []
+    changes_list: list[list[str]] = []
+
     try:
         with open(source_file) as csv_file:
             csv_as_list = list(csv.reader(csv_file))
-            used_ids: list[int] = []
             expected_width: int = len(csv_as_list[0])
 
             write_row(csv_as_list[0], target_file, used_ids, "w")
-
             for index, row in enumerate(csv_as_list[1:]):
-                corrected_row = error_handling(row, index + 1, used_ids, expected_width)
+                corrected_row = error_handling(
+                    row, index + 1, used_ids, expected_width, changes_list
+                )
                 write_row(corrected_row, target_file, used_ids)
+
+        with open(path.join("opg3", f"changes_log.txt"), "w") as changes_file:
+            changes_file.writelines(changes_list)
 
     except FileNotFoundError:
         print(f"Error: File {source_file} not found!")
